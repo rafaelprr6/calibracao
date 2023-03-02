16083d9a23da33a29db67234abdf0b7472c7cf6d
Rafael Pereira <rafael0prr@gmail.com>
2023-03-02 14:46:52 -0300

    Add files via upload

---
+++ deteccao_dicionario_aruco.py
@@ -0,0 +1,84 @@
+import cv2
+
+### Descição ###
+# Este código detecta o dicionário aruco em uma imagem utilizado para dar origem  
+# ao aruco. Ele está baseado no seguinte tutorial:
+# https://pyimagesearch.com/2020/12/28/determining-aruco-marker-type-with-opencv-and-python/
+
+
+# define names of each possible ArUco tag OpenCV supports
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+# resize the image
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
+
+# load the input image from disk and resize it
+print("[INFO] loading image...")
+image = cv2.imread("aruco.jpeg")
+
+image = resize(image, width=600)
+
+# loop over the types of ArUco dictionaries
+for (arucoName, arucoDict) in ARUCO_DICT.items():
+	# load the ArUCo dictionary, grab the ArUCo parameters, and
+	# attempt to detect the markers for the current dictionary
+	arucoDict = cv2.aruco.Dictionary_get(arucoDict)
+	arucoParams = cv2.aruco.DetectorParameters_create()
+	(corners, ids, rejected) = cv2.aruco.detectMarkers(
+		image, arucoDict, parameters=arucoParams)
+	# if at least one ArUco marker was detected display the ArUco
+	# name to our terminal
+	if len(corners) > 0:
+		print("[INFO] detected {} markers for '{}'".format(
+			len(corners), arucoName))
---
+++ deteccao_marcador_aruco.py
@@ -0,0 +1,83 @@
+import cv2
+from cv2 import aruco
+import numpy as np
+
+##### Descricao #####
+# Este programa detecta o id e as quinas de um aruco.
+# Ao final uma imagem é mostrada destacando o aruco,
+# como tambem seu id.
+
+# Funcao de redimensionamento da imagem
+
+
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
+
+
+# Dicionario Aruco
+marker_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+
+param_markers = aruco.DetectorParameters_create()
+
+img = cv2.imread("aruco.jpeg")
+img = resize(img, width=600)
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+marker_corners, marker_IDs, reject = aruco.detectMarkers(
+    gray_img, marker_dict, parameters=param_markers
+)
+if marker_corners:
+    for ids, corners in zip(marker_IDs, marker_corners):
+        cv2.polylines(
+            img, [corners.astype(np.int32)], True, (0,
+                                                    255, 255), 4, cv2.LINE_AA
+        )
+        corners = corners.reshape(4, 2)
+        corners = corners.astype(int)
+        top_right = corners[0].ravel()
+        top_left = corners[1].ravel()
+        bottom_right = corners[2].ravel()
+        bottom_left = corners[3].ravel()
+        cv2.putText(
+            img,
+            f"id: {ids[0]}",
+            top_right,
+            cv2.FONT_HERSHEY_PLAIN,
+            1.3,
+            (200, 100, 0),
+            2,
+            cv2.LINE_AA,
+        )
+        # print(ids, "  ", corners)
+
+cv2.imshow("frame", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
